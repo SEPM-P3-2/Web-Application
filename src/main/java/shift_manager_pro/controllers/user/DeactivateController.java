72544970998c3d2c270e33c6c3defa3d99713145
DAT LE <s3794387@student.rmit.edu.au>
2021-05-23 23:32:22 +1000

    fix delete user

--- src/main/java/shift_manager_pro/controllers/user/DeactivateController.java
+++ src/main/java/shift_manager_pro/controllers/user/DeactivateController.java
@@ -1,16 +1,16 @@
 package shift_manager_pro.controllers.user;
 
 
+import java.util.List;
+
+import org.jetbrains.annotations.NotNull;
+
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
-import org.jetbrains.annotations.NotNull;
-import org.mindrot.jbcrypt.BCrypt;
 import shift_manager_pro.dao.ShiftDao;
 import shift_manager_pro.dao.UserDao;
-import shift_manager_pro.models.Role;
 import shift_manager_pro.models.Shift;
 import shift_manager_pro.models.User;
-import shift_manager_pro.utils.Views;
 
 public class DeactivateController implements Handler{
     @Override
@@ -18,6 +18,10 @@ public class DeactivateController implements Handler{
 
 
         User user = UserDao.INSTANCE.get(ctx.pathParam("user_id", Long.class).get());
+        List<Shift> shifts  = ShiftDao.INSTANCE.getByUserId(user.getId());
+        for (Shift shift : shifts) {
+            ShiftDao.INSTANCE.delete(shift);
+        }
         UserDao.INSTANCE.delete(user);
 
         ctx.redirect( "/users/list");
